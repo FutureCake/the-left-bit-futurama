30acc5980db6e1feefd782eb932a1ca3453dcc63
FutureCake <thomas.c.hoek@gmail.com>
2025-08-07 16:35:07 +0200

    removed unused passport file

--- src/pages/character-details/components/passport.tsx
+++
@@ -1,9 +0,0 @@
-import type { CharacterPreviewProps } from "../../../shared/components/character-preview";
-import CharacterPreview from "../../../shared/components/character-preview";
-
-export default function Passport(props: CharacterPreviewProps) {
-
-    return (
-        <CharacterPreview {...props} />
-    )
-}
